a64260c04a67ffe74614b5b1d717c78003d93da0
abhinav6859 <abhinavvats06665@gmail.com>
2024-12-13 11:52:36 +0530

    name update

---
+++ 3_javascript/3.1_Introduction/3_operator.js
@@ -0,0 +1,12 @@
+// Bitwise Operator
+
+let x = 5; 
+let y = 3; 
+console.log(x & y);     //Bitwise AND
+console.log(x | y);     //Bitwise OR
+console.log(x ^ y);    //Bitwise XOR
+console.log(~10);      //Bitwise NOT
+
+let a = 4; 
+console.log(a << 1);    //Bitwise Left Shift
+console.log(a << 4);    //Bitwise Right Shift
