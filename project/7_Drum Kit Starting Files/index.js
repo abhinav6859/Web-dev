5dff3164b40e66f04bffc9f5b120466632f92a61
abhinav6859 <abhinavvats06665@gmail.com>
2024-12-21 16:50:18 +0530

    Arrange project

---
+++ project/7_Drum Kit Starting Files/index.js
@@ -0,0 +1,81 @@
+
+   
+ let drumButtons = document.querySelectorAll(".drum");
+
+ document.addEventListener("keydown", function(event) {
+     // Loop through all drum buttons
+     drumButtons.forEach(button => {
+         // Reset color for all buttons
+         button.style.color = "";
+ 
+         // Check if the button's text matches the pressed key
+         if (button.innerText.toLowerCase() === event.key.toLowerCase()) {
+             // Change the color of the matched button
+             button.style.color = "red";
+         }
+     });
+ });
+ 
+
+  
+     document.addEventListener("keydown",function(event){
+ 
+switch (event.key){
+  case "w":
+    var crash = new Audio('sounds/crash.mp3');
+    crash.play();
+    break;
+
+  case "a":
+    var kick = new Audio('sounds/kick-bass.mp3');
+    kick.play();
+    break;
+
+  case "s":
+    var snare = new Audio('sounds/snare.mp3');
+    snare.play();
+    break;
+
+  case "d":
+    var tom4 = new Audio('sounds/tom-4.mp3');
+    tom4.play();
+    break;
+
+  case "j":
+   
+    var tom3 = new Audio('sounds/tom-3.mp3');
+    tom3.play();
+    break;
+
+  case "k":
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+    break;
+  case "l":
+    var tom2 = new Audio("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+
+
+  default: console.log(event.key);
+
+}
+});
+
+document.addEventListener("keydown",function(event){
+
+
+  var activeButton = document.querySelector("." + event.key);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+
+}
+);
+
+  // var audio = new Audio("sounds/tom-1.mp3");
+  // audio.play();
