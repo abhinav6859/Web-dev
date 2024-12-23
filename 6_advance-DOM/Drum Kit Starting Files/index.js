8bcb3deab087dabc3899e0e06c309b15e23017a7
abhinav6859 <abhinavvats06665@gmail.com>
2024-12-23 23:06:32 +0530

    initial commit

---
+++ 6_advance-DOM/Drum Kit Starting Files/index.js
@@ -0,0 +1,12 @@
+// click button from js
+// document.querySelector("button").addEventListener("click",handleclick);
+// function handleclick(){
+//     alert("I got clicked!");
+
+// }
+      // OR
+      document.querySelector(".set").addEventListener("click",function(){
+    alert("I got clicked!");
+});
+$0.innerHTML="angela";
+
