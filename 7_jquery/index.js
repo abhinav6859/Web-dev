97d17b4edc593fceffa8435f1a7ad284ff5c38b3
abhinav6859 <abhinavvats06665@gmail.com>
2025-01-01 20:37:11 +0530

    initial commit

--- 7_jquery/index.js
+++ 7_jquery/index.js
@@ -1,4 +1,15 @@
 // $ or jQuery
+$("a").attr("href","https:www.google.com"); // change attribute
 
-alert("ram");
+$("h1").before("<button>new</button>");
+
+
 $("h1").css("color","white");
+
+$("body").keydown(function(event){
+
+$("h1").text(event.key);
+});
+
+
+//$("button").remove();      // remove all button
